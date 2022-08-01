43a5b4541b829725fa8bf00bc58c2eeeb02ccb41
Uli-mgh <ulicmgh@gmail.com>
2022-07-31 22:36:49 -0300

    deploy

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { getProducts } = require("./src/middlewares/middlewares");
 
+const { PORT } = process.env;
+
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, async () => {
+  server.listen(PORT, async () => {
     await getProducts();
-    console.log("%s listening at 3001");
+    console.log(`%s listening at ${PORT}`);
   });
 });
