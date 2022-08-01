132650692a7f26be436074ae35671fb739e95ca8
Uli-mgh <ulicmgh@gmail.com>
2022-08-01 01:48:58 -0300

    Error de pago

--- src/routes/pay.js
+++ src/routes/pay.js
@@ -1,83 +1,129 @@
 const { Router } = require("express");
 const Stripe = require("stripe");
 const { User, Product, Sell_order, Product_values } = require("../db");
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 const { Op } = require("sequelize");
-const stripe = new Stripe("sk_test_51LDapSLLyNiW7nbRtu012BcZsbgDoQtaLp5ADJ5usnS2kbDlUdBTda2fD0HqxN6PbBDUeQKTXFLRdxVZtntborIf00EcE31nIZ");
+//aca va la clave privada
+const stripe = new Stripe(
+  "sk_test_51LRM01FTo7BILoUXII9kTG5f0fYcBzL9P7w0ZxPiRCcJtVEIIyk61lM7yMwzmf8sdTL0JbjioaEcQvy3JHutCBa200eyDOEA8Z"
+);
+//
 const router = Router();
-const { mailPayment} = require("../middlewares/middlewares.js");
+const { mailPayment } = require("../middlewares/middlewares.js");
 //const Product_values = require("../models/Product_values");
 
 //
-function formatDescription(description){
-  return description.map(p=>p.name+' '+p.size+' '+p.quantity+' price '+p.price+' subTotal '+p.price*p.quantity)
+function formatDescription(description) {
+  return description.map(
+    (p) =>
+      p.name +
+      " " +
+      p.size +
+      " " +
+      p.quantity +
+      " price " +
+      p.price +
+      " subTotal " +
+      p.price * p.quantity
+  );
 }
 
-
 router.post("/api/checkout", async (req, res) => {
-    // you can get more data to find in a database, and so on
-    const { id, amount , description, user, shippingInfo} = req.body;
-    
-try {
-      const userComprador = await User.findByPk(user)//trae el user que compro, validar si no existe
-        if(user){
+  // you can get more data to find in a database, and so on
+  const { id, amount, description, user, shippingInfo } = req.body;
+  // console.log(description)
+  try {
+    const userComprador = await User.findByPk(user); //trae el user que compro, validar si no existe
+    if (user) {
       const payment = await stripe.paymentIntents.create({
-          amount:amount*100,
-          currency: "USD",
-          description: formatDescription(description).join(',\n'),
-          payment_method: id,
-          confirm: true, //confirm the payment at the same time
+        amount: amount * 100,
+        currency: "USD",
+        description: formatDescription(description).join(",\n"),
+        payment_method: id,
+        confirm: true, //confirm the payment at the same time
+      });
+
+      if (payment.status === "succeeded") {
+        const newSellOrder = await Sell_order.create({
+          amount: amount * 100,
+          product: formatDescription(description).join("\n"), //armar funcion
+          country: shippingInfo.country,
+          province: shippingInfo.province,
+          city: shippingInfo.city,
+          postalCode: shippingInfo.postalCode,
         });
-        
-        if(payment.status === 'succeeded'){
-          const newSellOrder = await Sell_order.create({
-            amount:amount*100,
-            product:formatDescription(description).join('\n'),//armar funcion 
-            country:shippingInfo.country,
-            province:shippingInfo.province,
-            city:shippingInfo.city,
-            postalCode:shippingInfo.postalCode
-          }) 
-
-          const userCompra = await Promise.all(description.map(async (p)=>{
-            return await Product.findByPk(p.id, {include:[{
-              model: Product_values,
-              where:{size:p.size},
-              attributes: ["id"],
-              through: { attributes: [] }
-            }]})
-          }))
-          //res.send(userCompra)
-          userCompra.map(async (prod, i)=>{
-            await Product_values.decrement(
-              'stock', 
-              {by:description[i].quantity,
-              where: {id:prod.product_values[0].id}})
+        // console.log(newSellOrder)
+        let userCompra = [];
+        if (description.length > 1) {
+          userCompra = await Promise.all(
+            description.map(async (p) => {
+              return await Product.findByPk(p.id, {
+                include: [
+                  {
+                    model: Product_values,
+                    where: { size: p.size },
+                    attributes: ["id"],
+                    through: { attributes: [] },
+                  },
+                ],
+              });
+            })
+          );
+          userCompra.map(async (prod, i) => {
+            await Product_values.decrement("stock", {
+              by: description[i].quantity,
+              where: { id: prod.product_values[0].id },
+            });
             //console.log(prod.product_values[0].id)
-          })
-          
-          let productosComprados = []
-          for (let i=0; i<userCompra.length; i++){
-            if(i===0) 
-              productosComprados.push(userCompra[i])
-            else if(userCompra[i].id!==userCompra[i-1].id)
-              productosComprados.push(userCompra[i])
-          }
-
-          await newSellOrder.addProducts(productosComprados)
-          
-          await userComprador.addSell_order(newSellOrder)
+          });
+        } else {
+          userCompra = await Product.findByPk(description[0].id, {
+            include: [
+              {
+                model: Product_values,
+                where: { size: description[0].size },
+                attributes: ["id"],
+                through: { attributes: [] },
+              },
+            ],
+          });
+          let aux = [];
+          aux.push(userCompra);
+          userCompra = aux;
         }
 
-        mailPayment(userComprador.dataValues.email, id, mensaje="Pago exitoso");
-      }     
-      else return res.json({ message: "hubo un error"})
-      
-      return res.status(200).json({ message: "Successful Payment" });
-    } catch (error) {
-      console.log(error);
-      return res.json({ message: "hubo un error"/* error.raw.message */ });
-    } 
-  }); 
+        userCompra.map(async (prod, i) => {
+          await Product_values.decrement("stock", {
+            by: description[i].quantity,
+            where: { id: prod.product_values[0].id },
+          });
+          //console.log(prod.product_values[0].id)
+        });
 
-  module.exports = router;
+        let productosComprados = [];
+        for (let i = 0; i < userCompra.length; i++) {
+          if (i === 0) productosComprados.push(userCompra[i]);
+          else if (userCompra[i].id !== userCompra[i - 1].id)
+            productosComprados.push(userCompra[i]);
+        }
+
+        await newSellOrder.addProducts(productosComprados);
+
+        await userComprador.addSell_order(newSellOrder);
+      }
+
+      mailPayment(
+        userComprador.dataValues.email,
+        id,
+        (mensaje = "Pago exitoso")
+      );
+    } else return res.json({ message: "hubo un error" });
+
+    return res.status(200).json({ message: "Successful Payment" });
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: "hubo un error" /* error.raw.message */ });
+  }
+});
+
+module.exports = router;
